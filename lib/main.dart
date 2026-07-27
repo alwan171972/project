1322e883345a8028e71b0c1317b03578a2c469c1
Alwan_Ali_Latief_Latief <alwan2604143@dugi-academy.id>
2026-07-27 04:27:06 +0000

    okeeee

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Coba());
+  runApp(Coba());
 }
 
 class Coba extends StatelessWidget {
-  const Coba({super.key});
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("3 Bagian Layar")),
-        body: Column(
-          children: [
-            // Bagian 1 - Atas (Merah)
-            Expanded(
-              child: Container(
-                width: double.infinity, // Memenuhi lebar layar
+        appBar: AppBar(
+          title: Text(
+            "gggg",
+            style: TextStyle(color: Colors.amberAccent),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                width: double.infinity,
                 color: Colors.redAccent,
-                child: const Center(
-                  child: Text(
-                    "kata 1",
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-
-            // Bagian 2 - Tengah (Hijau)
-            Expanded(
-              child: Container(
-                width: double.infinity, // Memenuhi lebar layar
-                color: Colors.green,
-                child: const Center(
-                  child: Text(
-                    "kata 2",
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-
-            // Bagian 3 - Bawah (Biru)
-            Expanded(
-              child: Container(
-                width: double.infinity, // Memenuhi lebar layar
-                color: Colors.blueAccent,
-                child: const Center(
-                  child: Text(
-                    "kata 3",
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-          ],
+              )),
+              Expanded(
+                  flex: 4,
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.blue,
+                  ))
+            ],
+          ),
         ),
       ),
     );
